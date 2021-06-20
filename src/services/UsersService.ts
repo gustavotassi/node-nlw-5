39982e9242af9528a83b1c680b0bd39960cf58c4
Gustavo Tassi <souzagustavotassi@gmail.com>
2021-06-20 15:38:38 -0300

    feat: add user controller, service, entity, repository and migration

---
+++ src/services/UsersService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    // Verificar se o usuário existe
+    const userAlreadyExists = await this.usersRepository.findOne({
+      email,
+    });
+
+    // Se existir, retornar user
+    if (userAlreadyExists) {
+      return userAlreadyExists;
+    }
+
+    // Se não existir, salvar no banco de dados
+    const user = this.usersRepository.create({
+      email,
+    });
+
+    await this.usersRepository.save(user);
+
+    // Retorna o user que foi criado
+    return user;
+  }
+}
+
+export { UsersService };
