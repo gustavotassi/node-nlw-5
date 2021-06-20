19c2a4effae0546806c5c74eade2b1f08bab329e
Gustavo Tassi <souzagustavotassi@gmail.com>
2021-06-20 14:06:41 -0300

    feat: add router and json config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import express from 'express';
-import './database';
+import express from "express";
+import { routes } from "./routes";
+import "./database";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log("Server is running on port 3000!");
 });
